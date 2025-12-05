1f4a9aedce05e371ebbee244099d1d86d7cfec7f
Ali <116540641+AliDemir140@users.noreply.github.com>
2025-12-05 09:47:42 +0300

    feat: delivery request dto'ları eklendi
    Closes #30

---
+++ Applications/Application/DTOs/DeliveryRequestDTOs/CreateDeliveryRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.DeliveryRequestDTOs
+{
+    public class CreateDeliveryRequestDto
+    {
+        [Required(ErrorMessage = "Üye bilgisi zorunludur.")]
+        public int MemberId { get; set; }
+
+        [Required(ErrorMessage = "Film listesi zorunludur.")]
+        public int MemberMovieListId { get; set; }
+
+        [Required(ErrorMessage = "Teslimat tarihi zorunludur.")]
+        public DateTime DeliveryDate { get; set; }
+    }
+}
---
+++ Applications/Application/DTOs/DeliveryRequestDTOs/DeliveryRequestItemDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application.DTOs.DeliveryRequestDTOs
+{
+    public class DeliveryRequestItemDto
+    {
+        public int Id { get; set; }
+
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+
+        public bool IsReturned { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public bool IsDamaged { get; set; }
+    }
+}
