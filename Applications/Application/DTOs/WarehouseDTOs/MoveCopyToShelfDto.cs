9f05015239bfddc8a1946e1ba622a8bcb157bb37
Ali <116540641+AliDemir140@users.noreply.github.com>
2025-12-12 10:03:27 +0300

    feat: warehouse service eklendi (raf envanteri ve iade işlemleri) #54

---
+++ Applications/Application/DTOs/WarehouseDTOs/MoveCopyToShelfDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.WarehouseDTOs
+{
+    public class MoveCopyToShelfDto
+    {
+        [Required]
+        public int MovieCopyId { get; set; }
+
+        [Required]
+        public int ShelfId { get; set; }
+    }
+}
---
+++ Applications/Application/DTOs/WarehouseDTOs/ProcessReturnDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.WarehouseDTOs
+{
+    public class ProcessReturnDto
+    {
+        [Required]
+        public int MovieCopyId { get; set; }
+
+        // Film bozuk mu geldi?
+        public bool IsDamaged { get; set; }
+
+        // Bozuksa açıklama
+        [MaxLength(500)]
+        public string? Note { get; set; }
+
+        // Raf değişikliği yapılacaksa
+        public int? TargetShelfId { get; set; }
+    }
+}
