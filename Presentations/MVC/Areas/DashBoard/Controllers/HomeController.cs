5c89085b7af143f89f699f216b514c2bb34eedf0
Ali <116540641+AliDemir140@users.noreply.github.com>
2025-12-14 13:30:08 +0300

    feat: MVC ortak layout ve CartSummary ViewComponent eklendi (closes #77)

---
+++ Presentations/MVC/Areas/DashBoard/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Areas.DashBoard.Controllers
+{
+    [Area("DashBoard")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
