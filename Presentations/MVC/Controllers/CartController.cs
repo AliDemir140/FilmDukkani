0d3490de9cae995f633c35b2358fcad1ca3c5373
Ali <116540641+AliDemir140@users.noreply.github.com>
2025-12-21 23:25:58 +0300

    Checkout teslimat isteği servise bağlandı

--- Presentations/MVC/Controllers/CartController.cs
+++ Presentations/MVC/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.ServiceManager;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MVC.Extensions;
 using MVC.Models;
 
@@ -9,10 +10,12 @@ namespace MVC.Controllers
     {
         private const string CartKey = "Cart";
         private readonly MovieServiceManager _movieService;
+        private readonly DeliveryRequestServiceManager _deliveryRequestService;
 
-        public CartController(MovieServiceManager movieService)
+        public CartController(MovieServiceManager movieService, DeliveryRequestServiceManager deliveryRequestService)
         {
             _movieService = movieService;
+            _deliveryRequestService = deliveryRequestService;
         }
 
         public IActionResult Index()
@@ -61,5 +64,65 @@ namespace MVC.Controllers
             HttpContext.Session.SetObject(CartKey, cart);
             return RedirectToAction("Index");
         }
+
+        public async Task<IActionResult> Checkout()
+        {
+            var cart = HttpContext.Session.GetObject<List<CartItem>>(CartKey)
+                       ?? new List<CartItem>();
+
+            if (!cart.Any())
+                return RedirectToAction("Index");
+
+            int memberId = 1; // şimdilik sabit
+
+            var lists = new List<SelectListItem>
+    {
+        new SelectListItem { Value = "1", Text = "Ana Liste" },
+        new SelectListItem { Value = "2", Text = "Yedek Liste" }
+    };
+
+            var model = new CheckoutViewModel
+            {
+                CartItems = cart,
+                MemberLists = lists,
+                SelectedListId = int.Parse(lists.First().Value), //
+                DeliveryDate = DateTime.Today.AddDays(2)
+            };
+
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Checkout(CheckoutViewModel model)
+        {
+            var cart = HttpContext.Session.GetObject<List<CartItem>>(CartKey)
+                       ?? new List<CartItem>();
+
+            if (!cart.Any())
+                return RedirectToAction("Index");
+
+            if (model.SelectedListId <= 0)
+            {
+                ModelState.AddModelError("", "Film listesi seçilmelidir.");
+                return View(model);
+            }
+
+            int memberId = 1; // auth gelene kadar sabit
+
+            await _deliveryRequestService.CreateDeliveryRequestAsync(
+                memberId,
+                model.SelectedListId,
+                model.DeliveryDate
+            );
+
+            //
+            HttpContext.Session.Remove(CartKey);
+
+            return RedirectToAction("Success");
+        }
+
+
     }
 }
---
+++ Presentations/MVC/Models/CheckoutViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MVC.Models
+{
+    public class CheckoutViewModel
+    {
+        public List<CartItem> CartItems { get; set; } = new();
+
+        public List<SelectListItem> MemberLists { get; set; } = new();
+
+        public int SelectedListId { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+    }
+}
